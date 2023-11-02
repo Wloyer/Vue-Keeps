00f07781c44d9c1a02becca3a0e3a30f882ee35b
William <w.loyer@outlook.com>
2023-11-02 11:39:42 +0100

    sass

--- src/components/mosaicPostIt.vue
+++ src/components/mosaicPostIt.vue
@@ -1,10 +1,10 @@
-<template>
-  <div class="postit-titre postit-container">
+<template class="mosaic">
+  <div class="mosaic__titre mosaic__container">
     <h1>Vos post-it :</h1>
   </div>
   <PostIt isInputMode />
-  <div class="postit-board postit-container">
-    <div class="postit-board-row">
+  <div class="mosaic__board mosaic__container">
+    <div class="mosaic__board-row">
       <div
         v-for="(postItsContent, index) in postItList"
         :key="index"
@@ -31,47 +31,48 @@ export default defineComponent({
   },
 });
 </script>
-<style scoped>
-.postit-titre {
-  height: 100%;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  padding: 50px;
-}
-.postit-board {
-  height: 100%;
-  width: 100%;
-  background-color: whitesmoke;
-  display: flex;
-  justify-content: center;
-  padding: 50px;
-}
 
-.postit-container {
-  width: 100%;
-  padding-right: 15px;
-  padding-left: 15px;
-  margin-right: auto;
-  margin-left: auto;
+<style lang="scss" scoped>
+.mosaic {
+  &__titre,
+  &__board {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    padding: 50px;
+  }
 
-  @media (min-width: 576px) {
-    width: 540px;
+  &__board {
+    background-color: whitesmoke;
   }
-  @media (min-width: 768px) {
-    width: 720px;
+
+  &__container {
+    width: 100%;
+    padding-right: 15px;
+    padding-left: 15px;
+    margin-right: auto;
+    margin-left: auto;
+
+    @media (min-width: 576px) {
+      width: 540px;
+    }
+    @media (min-width: 768px) {
+      width: 720px;
+    }
+    @media (min-width: 992px) {
+      width: 960px;
+    }
+    @media (min-width: 1200px) {
+      width: 1140px;
+    }
   }
-  @media (min-width: 992px) {
-    width: 960px;
+
+  &__board-row {
+    display: flex;
+    flex-wrap: wrap;
+    margin-right: -15px;
+    margin-left: -15px;
   }
-  @media (min-width: 1200px) {
-    width: 1140px;
-  }
-}
-.postit-board-row {
-  display: flex;
-  flex-wrap: wrap;
-  margin-right: -15px;
-  margin-left: -15px;
 }
 </style>
