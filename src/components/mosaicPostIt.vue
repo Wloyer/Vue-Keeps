33410f627b14ca46d4a6123b78c3cce388c62a12
William Loyer <w.loyer@outlook.com>
2023-11-03 16:48:32 +0100

    sass mixin

--- src/components/mosaicPostIt.vue
+++ src/components/mosaicPostIt.vue
@@ -1,28 +1,57 @@
 <template class="mosaic">
-  <div class="mosaic__titre mosaic__container">
+  <div class="mosaic__board mosaic__container">
     <h1>Vos post-it :</h1>
   </div>
   <slot name="postItInput"></slot>
   <div class="mosaic__board mosaic__container">
-    <div class="mosaic__board-row">
+    <div class="mosaic__board__row">
       <slot name="postItItems"></slot>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+@mixin respond-to($breakpoint) {
+  @if $breakpoint == sm {
+    @media (min-width: 576px) {
+      @content;
+    }
+  } @else if $breakpoint == md {
+    @media (min-width: 768px) {
+      @content;
+    }
+  } @else if $breakpoint == lg {
+    @media (min-width: 992px) {
+      @content;
+    }
+  } @else if $breakpoint == xl {
+    @media (min-width: 1200px) {
+      @content;
+    }
+  }
+}
+
+@mixin flex-center {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 50px;
+}
+
 .mosaic {
-  &__titre,
   &__board {
-    height: 100%;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    padding: 50px;
+    @include flex-center;
   }
 
   &__board {
     background-color: whitesmoke;
+    &__row {
+      display: flex;
+      flex-wrap: wrap;
+      margin-right: -15px;
+      margin-left: -15px;
+    }
   }
 
   &__container {
@@ -32,25 +61,18 @@
     margin-right: auto;
     margin-left: auto;
 
-    @media (min-width: 576px) {
+    @include respond-to(sm) {
       width: 540px;
     }
-    @media (min-width: 768px) {
+    @include respond-to(md) {
       width: 720px;
     }
-    @media (min-width: 992px) {
+    @include respond-to(lg) {
       width: 960px;
     }
-    @media (min-width: 1200px) {
+    @include respond-to(xl) {
       width: 1140px;
     }
-  }
-
-  &__board-row {
-    display: flex;
-    flex-wrap: wrap;
-    margin-right: -15px;
-    margin-left: -15px;
   }
 }
 </style>
